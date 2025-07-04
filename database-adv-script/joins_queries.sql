2621ea6b7617b22294aa4f5b90df9ae750b96db0
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2025-07-04 23:08:40 +0400

    fix: add missing ORDER BY clause in LEFT JOIN query for property reviews

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -37,6 +37,8 @@ FROM
     Properties AS p
 LEFT JOIN
     Reviews AS r ON p.property_id = r.property_id;
+ORDER BY
+    p.property_id;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking
 --    or a booking is not linked to a user.
